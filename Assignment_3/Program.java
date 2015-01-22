dbedcc1829fb1ee41c1b628b9cee6d23351df483
emilyshine <emilyshine1@gmail.com>
2015-01-21 18:40:00 -0800

    Create Program.java

---
+++ Assignment_3/Program.java
@@ -0,0 +1,18 @@
+/* ******************************************************
+ * Emily A. Shine
+ * January 21th, 2015
+ * Assignment 3 - Program.java
+ * This is the class from which the program is run.  
+ * ***************************************************** */
+
+package com.emilyshine.Assignment_3;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// instantiate the Display object.
+		Display d = new Display();
+
+	} // end main
+
+} // end Program
