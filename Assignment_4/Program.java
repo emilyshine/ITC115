8a27262eed27dd29c541b011e0e20fc142ef3e2d
emilyshine <emilyshine1@gmail.com>
2015-02-04 16:55:09 -0800

    Create Program.java

---
+++ Assignment_4/Program.java
@@ -0,0 +1,19 @@
+/* ******************************************************
+ * Emily A. Shine
+ * January 28th, 2015
+ * Assignment 4 - Program.java
+ * This is the class from which the program is run. 
+ * ***************************************************** */
+
+package com.emilyshine.Assignment_4;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// Instantiating display and calling Display constructor (from Display class)
+		Display d = new Display();
+
+	}
+
+}
