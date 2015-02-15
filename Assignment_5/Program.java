2d6913e5894c902adf02736c8a10cc6480dbf969
emilyshine <emilyshine1@gmail.com>
2015-02-15 14:31:57 -0800

    Create Program.java

---
+++ Assignment_5/Program.java
@@ -0,0 +1,19 @@
+/* ******************************************************
+ * Emily A. Shine
+ * February 12th, 2015
+ * Assignment 5 - Program.java
+ * This is the class from which the program is run. 
+ * ***************************************************** */
+
+package com.emilyshine.Assignment_5;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// Instantiates and constructs the Display class, declares d a new Display
+		Display d = new Display();
+
+	} // end main
+
+}
