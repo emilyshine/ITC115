085ec966a5c8142a3de19d8770219798e147f619
emilyshine <emilyshine1@gmail.com>
2015-02-15 15:03:45 -0800

    Create Program.java

---
+++ Quiz_3/Program.java
@@ -0,0 +1,17 @@
+/* ******************************************************
+ * Emily A. Shine
+ * February 15th, 2015
+ * Quiz 3 - Program.java
+ * This is the class from which the program is run.  
+ * ***************************************************** */
+
+package com.emilyshine.Quiz_3;
+
+public class Program {
+
+	public static void main(String[] args) {
+		FormLayout fl = new FormLayout();
+
+	}
+
+}
