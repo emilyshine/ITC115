d818dab29081e61143b5ced92aa52b3f755b8bd0
emilyshine <emilyshine1@gmail.com>
2015-03-04 19:02:56 -0800

    Update and rename Assignment_6 to Assignment_6/ManageItems.java

---
+++ Assignment_6/ManageItems.java
@@ -0,0 +1,18 @@
+/* ******************************************************
+ * Emily A. Shine
+ * February 18th, 2015
+ * Assignment 6 - ManageItems.java
+ * This is the ManageItems class.
+ * It will be the interface used to describe the three
+ * methods used within the classes.  
+ * ***************************************************** */
+
+package com.emilyshine.Assignment_6;
+
+public interface ManageItems {
+	
+	void addItem(Item i);
+	void editItem(Item i);
+	void removeItem(Item i);
+
+} // end ManageItems interface
