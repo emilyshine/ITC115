be21dff49d682ab0d6bb11aa1f9ed65d942b88df
emilyshine <emilyshine1@gmail.com>
2015-03-04 18:56:12 -0800

    Create Program.java

---
+++ Assignment_6/Program.java
@@ -0,0 +1,21 @@
+/* ******************************************************
+ * Emily A. Shine
+ * February 18th, 2015
+ * Assignment 6 - Program.java
+ * This is the main Program.  It calls the display 
+ * classes and runs the program.    
+ * ***************************************************** */
+
+package com.emilyshine.Assignment_6;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Recipes recipes = new Recipes();
+		
+		NewRecipe n = new NewRecipe();
+		//LookupRecipe l = new LookupRecipe(recipes);
+
+	} // end Main
+
+} // end Program 
