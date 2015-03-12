1577ac7b93648898e4c229398d1d0000cebb8970
emilyshine <emilyshine1@gmail.com>
2015-03-12 10:12:52 -0700

    Create Sounds.java

---
+++ Quiz_4/Sounds.java
@@ -0,0 +1,16 @@
+/* ******************************************************
+ * Emily A. Shine
+ * March 12th, 2015
+ * Quiz 4 - Sounds.java
+ * This is the Sounds class. 
+ * The interface provides the makeSound() 
+ * method to all implementing classes.  
+ * ***************************************************** */
+
+package com.emilyshine.Quiz_4;
+
+public interface Sounds {
+
+	String makeSound();
+	
+} // end Sounds interface
